2e809adf90488fb37c4e2f59faab5f8715a7d57f
Ishaan Sathaye <Ishaan.sathaye@gmail.com>
2020-10-26 20:47:10 -0700

    final update

--- ODE Test RK4/RKFinal.m
+++ ODE Test RK4/RKFinal.m
@@ -1,3 +1,4 @@
+%%APPROXIMATE%%
 %Constant Values
 radius = 0.001; 
 uf = 0.084;
@@ -14,23 +15,15 @@ k2 = 1 - k1;
 X_1 = @(t)(MD/F) + k1 * F * exp(-F*t);
 X = @(t)((MD/F) * t + k1 * exp(-F*t) + k2);
 newY = @(x) (x*(0.2087) + 0.0049*exp(-42.1875*x) + 0.9951);
-
-% syms t;
-% E = (t * (MD/F) + k1 * exp(-F*t) + k2) == 0;
-% E = (1 + (t *(-F/MD) + k1 * (1/(-exp(-F*t))) + k2 == 0));
-% T1 = vpasolve(E, t);
-% T1
-% syms x;
-% S = vpasolve(x+1 == 2, x);
-% S
-apprx = fplot(X, [0, 50]);
+tBottom = fzero(@(x)newY(x)-9, 39)
+apprx = fplot(X, [0, tBottom]);
 hold on;
 
-
+%%TRUE%%
 Ts = [0.1, 1, 10];
 for n = 1:3
     deltaT = Ts(n);
-    t = 0:deltaT:50;
+    t = 0:deltaT:tBottom;
     XTrue = zeros(1,length(t)); 
     XTrue(1) = 1;
     for i=1:(length(t)-1)
@@ -40,14 +33,14 @@ for n = 1:3
         k_4 = X_1(t(i)+deltaT);
         XTrue(i+1) = XTrue(i) + (1/6)*(k_1+2*k_2+2*k_3+k_4)*deltaT;
     end
-%     if deltaT == 0.1
-%         Tplot1 = plot(t, XTrue);
-%     elseif deltaT == 1
-%         Tplot2 = plot(t, XTrue);
-%     elseif deltaT == 10
-%         Tplot3 = plot(t, XTrue);
-%     end
+    if deltaT == 0.1
+        Tplot1 = plot(t, XTrue);
+    elseif deltaT == 1
+        Tplot2 = plot(t, XTrue);
+    elseif deltaT == 10
+        Tplot3 = plot(t, XTrue);
+    end
 end
-% legend([apprx Tplot1 Tplot2 Tplot3], ["Approximate", "True-0.1s", "True-1s", "True-10s"])
+legend([apprx Tplot1 Tplot2 Tplot3], ["Approximate", "True-0.1s", "True-1s", "True-10s"])
 hold on;
 hold off;
